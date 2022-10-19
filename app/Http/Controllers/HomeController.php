f205bb677dd0eddcf2e07e7d8cb87e91ea013254
a.karamiani <aziz.karamiani@gmail.com>
2022-10-20 00:11:16 +0330

    Show popular posts in homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Database\Eloquent\Builder;
 
 class HomeController extends Controller
 {
@@ -19,10 +21,12 @@ class HomeController extends Controller
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * @return Renderable
      */
     public function index()
     {
-        return view('home');
+        $posts = Post::with('community')->withCount('votes')->take(10)->get();
+
+        return view('home', compact('posts'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,31 @@
 
 @section('content')
     <div class="card">
-        <div class="card-header">{{ __('Dashboard') }}</div>
+        <div class="card-header">{{ __('Popular Posts') }}</div>
 
         <div class="card-body">
-            @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
+            @foreach($posts as $post)
+                <div class="row">
+                    <div
+                        class="col-md-1 d-flex flex-column justify-content-center align-content-center mb-2">
+                        <a href="{{ route('posts.vote', [$post->id, 1]) }}" class="text-center">
+                            <span class="fa fa-2x fa-sort-asc"></span>
+                        </a>
+                        <b class="text-center">{{ $post->votes }}</b>
+                        <a href="{{ route('posts.vote', [$post->id, -1]) }}" class="text-center">
+                            <span class="fa fa-2x fa-sort-desc"></span>
+                        </a>
+                    </div>
+                    <div class="col-md-11">
+                        <a href="{{ route("communities.posts.show", [$post->community, $post]) }}" class="link">
+                            <h2>{{ $post->title }}</h2>
+                        </a>
+                        <p>{{ $post->created_at->diffForHumans() }}</p>
+                        <p>{{ \Illuminate\Support\Str::words($post->post_text, 10) }}</p>
+                    </div>
+                    <hr>
                 </div>
-            @endif
-
-            {{ __('You are logged in!') }}
+            @endforeach
         </div>
     </div>
 @endsection
